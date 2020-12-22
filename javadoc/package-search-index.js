f1151a367998109a853d1b834e912b178e24211a
Matt E <matoos32@users.noreply.github.com>
2020-12-22 13:58:51 -0500

    Generate documentation for v1.5.5

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.jfcbuilder.builders"},{"l":"com.jfcbuilder.types"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jfcbuilder.builders"},{"l":"com.jfcbuilder.types"}];updateSearchResults();
